4d73b5a6b425f2ab940c9c645423f2f95139a010
zzuxyj <aayxyj@163.com>
2021-08-03 22:45:18 +0800

    springcloud-netflix - eureka ribbon feign hystrix zuul config

---
+++ TestSpringCloud01/Springcloud-provider-dept-hystrix-8001/src/main/java/cn/edu/zzu/controller/DeptController.java
@@ -0,0 +1,33 @@
+package cn.edu.zzu.controller;
+
+import cn.edu.zzu.domain.Dept;
+import cn.edu.zzu.serrvice.IDeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dept")
+public class DeptController {
+    @Autowired
+    private IDeptService iDeptService;
+
+    @GetMapping("/get/{id}")
+    @HystrixCommand(fallbackMethod = "hystrixGet")  //方法服务熔断
+    public Dept getDeptById(@PathVariable("id") Long id) {
+        Dept deptById = iDeptService.findDeptById(id);
+        if (deptById == null) {
+            throw new RuntimeException("id==>"+id+"不存在该用户，或者信息无法找到~");
+        }
+        return deptById;
+    }
+
+    public Dept hystrixGet(@PathVariable("id") Long id){
+        return new Dept().setDeptno(id)
+                .setDname("id==>"+id+"不存在该用户，或者信息无法找到~ Null-@HyStrix")
+                .setDb_source("no this database in MySQL!");
+    }
+
+
+}
+
